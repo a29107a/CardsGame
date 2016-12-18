9c7e5f297434459238f19696c505d352597db37d
Anthony Jiang <2nth0nyj@gmail.com>
2016-12-19 04:38:50 +0800

    commit proto and tools.

---
+++ proto/generate_protocol.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+[ -d include ] || mkdir -pv include
+[ -d src/pb ] || mkdir -pv src/pb
+
+
+for f in $(ls *.proto)
+do
+../deps/gpb/bin/protoc-erl -defaults-for-omitted-optionals -pldefs -I ./ -o-hrl include -o-erl src/pb ${f}
+done
+
+erl -make
