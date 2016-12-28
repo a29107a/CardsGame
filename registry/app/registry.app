6bb86e66ca6bb5eafb4dd6599495e135cfd0a132
Anthony Jiang <2nth0nyj@gmail.com>
2016-12-28 13:46:23 +0800

    add registry server.

---
+++ registry/app/registry.app
@@ -0,0 +1,11 @@
+{application, registry, [
+  {description, ""},
+  {vsn, "1"},
+  {registered, []},
+  {applications, [
+    kernel,
+    stdlib
+  ]},
+  {mod, {registry_app, []}},
+  {env, []}
+]}.
---
+++ registry/src/registry_sup.erl
@@ -0,0 +1,25 @@
+-module(registry_sup).
+-behaviour(supervisor).
+
+-export([start_link/0]).
+
+-export([init/1]).
+
+start_link() ->
+  supervisor:start_link({local, ?MODULE}, ?MODULE, []).
+
+init([]) ->
+  RestartStrategy = one_for_one,
+  MaxRestarts = 10000,
+  MaxSecondsBetweenRestarts = 36000,
+
+  SupFlags = {RestartStrategy, MaxRestarts, MaxSecondsBetweenRestarts},
+
+  Restart = permanent,
+  Shutdown = 20000,
+  Type = worker,
+
+  RegistedDbServer = {'registered_db_server', {'registered_db_server', start_link, []},
+    Restart, Shutdown, Type, ['registered_db_server']},
+
+  {ok, {SupFlags, [RegistedDbServer]}}.
