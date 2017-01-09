25ebc90bfabad318e92e08db3dd6398ee99c1485
Anthony Jiang <2nth0nyj@gmail.com>
2017-01-09 22:30:45 +0800

    build_all.sh execute faster with parallel buiding.

--- build_all.sh
+++ build_all.sh
@@ -1,35 +1,29 @@
 #!/usr/bin/env bash
 
 cd proto
-rm -frv include ebin src/pb include
-sh generate_protocol.sh
+rm -fr include ebin src/pb include && sh generate_protocol.sh
 cd ..
 
 cd shared
-rm -frv ebin
-sh build.sh
+rm -fr ebin && sh build.sh &
 cd ..
 
 cd connector
-rm -frv ebin
-sh build.sh
+rm -fr ebin && sh build.sh &
 cd ..
 
 cd db_server
-rm -frv ebin
-sh build.sh
+rm -fr ebin && sh build.sh &
 cd ..
 
 cd game_hall
 
 cd login_server
-rm -frv ebin
-sh build.sh
+rm -fr ebin && sh build.sh &
 cd ..
 
 cd game_server_center
-rm -frv ebin
-sh build.sh
+rm -fr ebin && sh build.sh &
 cd ..
 
 cd ..
@@ -37,12 +31,14 @@ cd ..
 cd games
 
 cd game_center_agent
-sh build.sh
+rm -fr ebin && sh build.sh &
 cd ..
 
 cd ..
 
 cd registry
-rm -frv ebin
-sh build.sh
+rm -fr ebin && sh build.sh &
 cd ..
+
+echo "all building process started!"
+echo ""
